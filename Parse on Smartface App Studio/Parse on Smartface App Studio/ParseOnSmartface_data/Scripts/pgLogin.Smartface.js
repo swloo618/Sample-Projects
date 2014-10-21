9b16c536f35516992adb86e0ed19b782d5591e39
Tolga Haliloğlu <tolga.haliloglu.28@gmail.com>
2014-10-21 15:36:38 +0300

    New Parse Project

---
+++ Parse on Smartface App Studio/Parse on Smartface App Studio/ParseOnSmartface_data/Scripts/pgLogin.Smartface.js
@@ -0,0 +1,6 @@
+function pgLogin_Self_OnKeyPress(e) {
+    if (e.keyCode === 4) {
+        Application.exit();
+    }
+}
+
---
+++ Parse on Smartface App Studio/Parse on Smartface App Studio/ParseOnSmartface_data/Scripts/pgUser.Smartface.js
@@ -0,0 +1,6 @@
+function pgUser_Self_OnKeyPress(e) {
+    if (e.keyCode === 4) {
+        Pages.back();
+    }
+}
+
